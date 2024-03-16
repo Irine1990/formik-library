6a18247bda4809d12d43f906764a7a4ead718c35
Irine1990 <Irine1990@gmail.com>
2024-03-16 22:42:40 +0530

    formik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./Components/Books";
+import Header from "./Components/Header";
+import Author from "./Components/Author";
+import Homepage from "./Pages/HomePage";
+import BooksCollection from "./Pages/BooksPage";
+import AuthorCollection from "./Pages/AuthorPage";
+import { useState } from "react";
+import Footer from "./Components/Footer";
 function App() {
+  const [data, setData] = useState([]);
+  const [edit, setEdit] = useState({
+    id: "",
+    title: "",
+    author: "",
+    isbn: "",
+    data: "",
+    isEditing: false,
+  });
+
+  const [author, setAuthor] = useState([]);
+  const [authedit, setAuthEdit] = useState({
+    id: "",
+    name: "",
+    date: "",
+    bio: "",
+    isEditing: false,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route
+          path="/Books"
+          element={
+            Object.keys(edit).length > 0 ? (
+              <Books setData={setData} data={data} edit={edit} />
+            ) : (
+              ""
+            )
+          }
+        />
+        <Route
+          path="/Author"
+          element={
+            Object.keys(authedit).length > 0 ? (
+              <Author
+                setAuthor={setAuthor}
+                setAuthEdit={setAuthEdit}
+                author={author}
+                authedit={authedit}
+              />
+            ) : (
+              ""
+            )
+          }
+        />
+        <Route
+          path="/BooksPage"
+          element={
+            <BooksCollection
+              data={data}
+              setData={setData}
+              edit={edit}
+              setEdit={setEdit}
+            />
+          }
+        />
+        <Route
+          path="/AuthorPage"
+          element={
+            <AuthorCollection
+              author={author}
+              setAuthor={setAuthor}
+              authedit={authedit}
+              setAuthEdit={setAuthEdit}
+            />
+          }
+        />
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
   );
 }
 
